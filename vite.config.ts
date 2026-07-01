05ffd78c927b53f69404fec2091d298192cc131b
kanakanho <kahoshibakichi@gmail.com>
2026-07-01 22:48:55 +0900

    init: github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [],
-  base: process.env.GITHUB_PAGES ? 'wgsl-template' : './',
+  base: process.env.GITHUB_PAGES ? 'music2waveform' : './',
 })
